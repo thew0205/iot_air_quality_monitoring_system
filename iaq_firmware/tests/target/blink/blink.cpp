98925b11ae2b49bf03e0d5d8c59874986abf6ba3
thew0205 <busoyetm@gmail.com>
2025-09-10 22:58:38 +0100

    Added the freertos to the project

--- iaq_firmware/tests/target/blink/blink.cpp
+++ iaq_firmware/tests/target/blink/blink.cpp
@@ -6,6 +6,10 @@
 
 #include "pico/stdlib.h"
 #include "wifi.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
 // Pico W devices use a GPIO on the WIFI chip for the LED,
 // so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
 #ifdef CYW43_WL_GPIO_LED_PIN
@@ -17,7 +21,8 @@
 #endif
 
 // Perform initialisation
-int pico_led_init(void) {
+int pico_led_init(void)
+{
 #if defined(PICO_DEFAULT_LED_PIN)
     // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
     // so we can use normal GPIO functionality to turn the led on and off
@@ -31,7 +36,8 @@ int pico_led_init(void) {
 }
 
 // Turn the led on or off
-void pico_set_led(bool led_on) {
+void pico_set_led(bool led_on)
+{
 #if defined(PICO_DEFAULT_LED_PIN)
     // Just set the GPIO on or off
     gpio_put(PICO_DEFAULT_LED_PIN, led_on);
@@ -41,12 +47,141 @@ void pico_set_led(bool led_on) {
 #endif
 }
 
-int main() {
+void mainTask2(void *param)
+{
 
     stdio_init_all();
     int rc = pico_led_init();
     hard_assert(rc == PICO_OK);
-    while (true) {
+    while (true)
+    {
         reConnect();
+        vTaskDelay(
+            2000);
+        
+    }
+}
+
+/*
+ * main.cpp
+ *
+ *  Created on: 21-Sep-2024
+ *      Author: jondurrant
+ */
+
+#include <cstdio>
+
+// Standard Task priority
+#define TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
+
+#define GP_BLINK 2
+
+/***
+ * Debug function to look at Task Stats
+ */
+void runTimeStats()
+{
+    TaskStatus_t *pxTaskStatusArray;
+    volatile UBaseType_t uxArraySize, x;
+    unsigned long ulTotalRunTime;
+
+    // Get number of takss
+    uxArraySize = uxTaskGetNumberOfTasks();
+    printf("Number of tasks %d\n", uxArraySize);
+
+    // Allocate a TaskStatus_t structure for each task.
+    pxTaskStatusArray = (TaskStatus_t *)pvPortMalloc(uxArraySize * sizeof(TaskStatus_t));
+
+    if (pxTaskStatusArray != NULL)
+    {
+        // Generate raw status information about each task.
+        uxArraySize = uxTaskGetSystemState(pxTaskStatusArray,
+                                           uxArraySize,
+                                           &ulTotalRunTime);
+
+        // Print stats
+        for (x = 0; x < uxArraySize; x++)
+        {
+            printf("Task: %d \t cPri:%d \t bPri:%d \t hw:%d \t%s\n",
+                   pxTaskStatusArray[x].xTaskNumber,
+                   pxTaskStatusArray[x].uxCurrentPriority,
+                   pxTaskStatusArray[x].uxBasePriority,
+                   pxTaskStatusArray[x].usStackHighWaterMark,
+                   pxTaskStatusArray[x].pcTaskName);
+        }
+
+        // Free array
+        vPortFree(pxTaskStatusArray);
+    }
+    else
+    {
+        printf("Failed to allocate space for stats\n");
+    }
+
+    // Get heap allocation information
+    HeapStats_t heapStats;
+    vPortGetHeapStats(&heapStats);
+    printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
+           heapStats.xAvailableHeapSpaceInBytes,
+           heapStats.xNumberOfFreeBlocks,
+           heapStats.xNumberOfSuccessfulAllocations,
+           heapStats.xNumberOfSuccessfulFrees);
+}
+
+/***
+ * Main task to boot the other Agents
+ * @param params - unused
+ */
+void mainTask(void *params)
+{
+    auto res = xTaskCreate(
+        mainTask2, /* Function that implements the task. */
+        "pName",   /* Text name for the task. */
+        10000,     /* Stack size in words, not bytes. */
+        NULL,      /* Parameter passed into the task. */
+        2,  /* Priority at which the task is created. */
+        NULL);
+
+    // printf("Boot task started\n");
+
+    for (;;)
+    {
+        // reConnect();
+
+        vTaskDelay(2000);
+        runTimeStats();
+    }
+}
+
+/***
+ * Launch the tasks and scheduler
+ */
+void vLaunch(void)
+{
+
+    // Start blink task
+    TaskHandle_t task;
+    xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
+
+    /* Start the tasks and timer running. */
+    vTaskStartScheduler();
+}
+
+int main()
+{
+
+    stdio_init_all();
+    sleep_ms(1000);
+
+    printf("Go\n");
+
+    // Start tasks and scheduler
+    const char *rtos_name = "FreeRTOS";
+    printf("Starting %s on core 0:\n", rtos_name);
+    vLaunch();
+
+    for (;;)
+    {
+        sleep_ms(3000);
     }
 }
